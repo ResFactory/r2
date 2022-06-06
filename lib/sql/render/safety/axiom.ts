08f0e1b0aac15f1caabadda34d57d8080ae5948d
Shahid N. Shah <shahid@shah.org>
2022-06-06 11:10:18 -0400

    feat: introduce type-safe data domains

---
+++ lib/sql/render/safety/axiom.ts
@@ -0,0 +1,304 @@
+// Extracted from https://github.com/tinysource/tinysource/tree/main/schema
+// and renamed to "Axiom" instead of "Schema" since schema is easily confused
+// with RDBMS Schema and SQL DDL.
+
+// Changes:
+// - Renamed Schema* to Axiom*
+// - Changed AxiomObjectType to AxiomObjectTypeStrict and forced default strict
+// - changed test cases to use Deno unit testing
+
+// deno-lint-ignore-file no-explicit-any
+type Primitive = bigint | boolean | number | string | symbol | null | undefined;
+
+type UnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends
+  (x: infer V) => any ? V : never;
+
+type OptionalKeys<T> = {
+  readonly [P in keyof T]: undefined extends T[P] ? P : never;
+}[keyof T];
+type RequiredKeys<T> = {
+  readonly [P in keyof T]: undefined extends T[P] ? never : P;
+}[keyof T];
+type SmartPartial<T> =
+  & Partial<Pick<T, OptionalKeys<T>>>
+  & Pick<T, RequiredKeys<T>>;
+
+// eslint-disable-next-line functional/prefer-readonly-type
+type Simplify<T> = T extends Record<string, unknown> ? { [P in keyof T]: T[P] }
+  : T;
+
+/**
+ * Infer the parsed type from a type-safe structure ("axiom").
+ */
+type AxiomType<TAxiom> = TAxiom extends Axiom<infer TType> ? TType : never;
+// eslint-disable-next-line functional/prefer-readonly-type
+type AxiomTupleType<TAxioms> = { [P in keyof TAxioms]: AxiomType<TAxioms[P]> };
+
+// "strict" objects allow only build-time defined properties
+// the original tinysource/schema did not have strict objects
+type AxiomObjectTypeStrict<TAxioms, TIndexType> = SmartPartial<
+  AxiomTupleType<TAxioms>
+>;
+
+// "relaxed" objects allow properties to be defined at run-time
+// the original tinysource/schema allowed relaxed only
+type AxiomObjectTypeRelaxed<TAxioms, TIndexType> =
+  & Record<string, TIndexType>
+  & SmartPartial<AxiomTupleType<TAxioms>>;
+
+type AxiomOptions = {
+  readonly onInvalid?: (reason: string) => void;
+  readonly path?: readonly (number | string)[];
+};
+
+type AxiomContext = {
+  readonly onInvalid: (reason?: string) => void;
+  /**
+   * Readonly string/number array which stringifies to a JSON path.
+   */
+  readonly path: readonly (number | string)[];
+};
+
+type Axiom<TType> = {
+  /**
+   * Shortcut to union this axiom with `$.undefined`.
+   */
+  readonly optional: () => Axiom<TType | undefined>;
+  /**
+   * Return the unmodified `value` if it matches the axiom. Otherwise, throw
+   * an error.
+   */
+  readonly parse: (value: unknown) => TType;
+  /**
+   * Returns true if the `value` matches the axiom. Otherwise, return false.
+   * The `onInvalid` callback will be called for each JSON path that does not
+   * match the axiom.
+   */
+  readonly test: (value: unknown, options?: AxiomOptions) => value is TType;
+};
+
+type AxiomObject<TType> = Axiom<TType> & {
+  /**
+   * Re-construct the object/record axiom with all properties as optional.
+   */
+  readonly partial: () => AxiomObject<Simplify<Partial<TType>>>;
+};
+
+const isObject = (value: any): value is Record<string, any> =>
+  typeof value === "object" && value !== null;
+
+const lazy = <TType>(callback: () => TType): (() => TType) => {
+  let cached: { value: TType } | undefined;
+
+  return () => {
+    if (!cached) {
+      cached = { value: callback() };
+    }
+
+    return cached.value;
+  };
+};
+
+const create = <TType>(
+  test: (value: unknown, context: AxiomContext) => value is TType,
+): Axiom<TType> => {
+  const axiom: Axiom<TType> = {
+    optional: lazy(() => $.union(axiom, $.undefined)),
+    parse: (value) => {
+      axiom.test(value, {
+        onInvalid: (reason) => {
+          throw new Error(reason);
+        },
+      });
+
+      return value as TType;
+    },
+    test: (value, { onInvalid, path = [] } = {}): value is TType => {
+      let valid = true;
+      let pathString: string | undefined;
+
+      const context: AxiomContext = {
+        onInvalid: (reason) => {
+          valid = false;
+
+          if (onInvalid) {
+            onInvalid(reason ?? `Unexpected type at ${context.path}`);
+          }
+        },
+        path: Object.assign(path, {
+          toString: () => {
+            if (pathString == null) {
+              pathString = path.reduce<string>((result, segment) => {
+                return (
+                  result +
+                  (typeof segment === "string" && /^[a-z0-9_]+$/gi.test(segment)
+                    ? `.${segment}`
+                    : `[${JSON.stringify(segment)}]`)
+                );
+              }, "$");
+            }
+
+            return pathString;
+          },
+        }),
+      };
+
+      const success = test(value, context);
+
+      if (!success && valid) {
+        context.onInvalid();
+      }
+
+      return valid;
+    },
+  };
+
+  return axiom;
+};
+
+const createObject = <
+  TPropAxioms extends Record<string, Axiom<any>>,
+  TIndexType,
+>(
+  props: TPropAxioms,
+  index: Axiom<TIndexType> | undefined,
+): AxiomObject<Simplify<AxiomObjectTypeStrict<TPropAxioms, TIndexType>>> => {
+  const objectAxiom: AxiomObject<
+    Simplify<AxiomObjectTypeStrict<TPropAxioms, TIndexType>>
+  > = {
+    ...create(
+      (
+        value,
+        { path, ...context },
+      ): value is Simplify<AxiomObjectTypeStrict<TPropAxioms, TIndexType>> =>
+        isObject(value) &&
+        Object.entries(props).every(([key, axiom]) =>
+          axiom.test(value[key], { ...context, path: [...path, key] })
+        ) &&
+        (!index ||
+          Object.entries(value).every(
+            ([key, value_]) =>
+              key in props ||
+              index.test(value_, { ...context, path: [...path, key] }),
+          )),
+    ),
+    partial: lazy(() => {
+      const partialProps: Record<string, Axiom<any>> = {};
+      Object.entries(props).forEach((
+        [key, axiom],
+      ) => (partialProps[key] = axiom.optional()));
+      return createObject(partialProps, index && index.optional()) as never;
+    }),
+  };
+
+  return objectAxiom;
+};
+
+// Memoized
+const string = create((value): value is string =>
+  typeof value === "string" || value instanceof String
+);
+const number = create((value): value is number =>
+  typeof value === "number" || value instanceof Number
+);
+const bigint = create((value): value is bigint =>
+  typeof value === "bigint" || value instanceof BigInt
+);
+const boolean = create((value): value is boolean =>
+  typeof value === "boolean" || value instanceof Boolean
+);
+const symbol = create((value): value is symbol =>
+  typeof value === "symbol" || value instanceof Symbol
+);
+const undefined_ = create((value): value is undefined =>
+  typeof value === "undefined"
+);
+const null_ = create((value): value is null => value === null);
+const unknown = create((_value): _value is unknown => true);
+const any = unknown as Axiom<any>;
+const function_ = create(
+  (value): value is (...args: unknown[]) => unknown =>
+    typeof value === "function" || value instanceof Function,
+);
+
+// Non-memoized
+const instance = <TType>(constructor: new (...args: readonly any[]) => TType) =>
+  create((value): value is TType => value instanceof constructor);
+const enum_ = <TType extends readonly [Primitive, ...(readonly Primitive[])]>(
+  ...values: TType
+) => create((value): value is TType[number] => values.includes(value as never));
+const array = <TType>(axiom?: Axiom<TType>) =>
+  create(
+    (value, { path, ...context }): value is TType[] =>
+      Array.isArray(value) &&
+      (!axiom ||
+        value.every((item, index) =>
+          axiom.test(item, { ...context, path: [...path, index] })
+        )),
+  );
+const tuple = <TAxioms extends readonly Axiom<any>[]>(...elements: TAxioms) =>
+  create(
+    (value, { path, ...context }): value is AxiomTupleType<TAxioms> =>
+      Array.isArray(value) &&
+      elements.length === value.length &&
+      elements.every((element, index) =>
+        element.test(value[index], { ...context, path: [...path, index] })
+      ),
+  );
+const record = <TType>(index?: Axiom<TType>) => createObject({}, index);
+const object = <TPropAxioms extends Record<string, Axiom<any>>, TIndexType>(
+  props: TPropAxioms,
+  index?: Axiom<TIndexType>,
+) => createObject(props, index);
+
+// Combinatorial
+const union = <
+  TAxioms extends readonly [Axiom<any>, ...(readonly Axiom<any>[])],
+>(...axioms: TAxioms) =>
+  create((value): value is AxiomType<TAxioms[number]> =>
+    axioms.some((axiom) => axiom.test(value))
+  );
+const intersection = <
+  TAxioms extends readonly [Axiom<any>, ...(readonly Axiom<any>[])],
+>(...axioms: TAxioms) =>
+  create((
+    value,
+    context,
+  ): value is UnionToIntersection<AxiomType<TAxioms[number]>> =>
+    axioms.every((axiom) => axiom.test(value, context))
+  );
+
+const $ = {
+  ...{
+    any,
+    array,
+    bigint,
+    boolean,
+    custom: create,
+    enum: enum_,
+    function: function_,
+    instance,
+    intersection,
+  },
+  ...{
+    null: null_,
+    number,
+    object,
+    record,
+    string,
+    symbol,
+    tuple,
+    undefined: undefined_,
+    union,
+    unknown,
+  },
+} as const;
+
+export {
+  $,
+  type Axiom,
+  type AxiomContext,
+  type AxiomObject,
+  type AxiomOptions,
+  type AxiomType,
+};
---
+++ lib/sql/render/safety/axiom_test.ts
@@ -0,0 +1,182 @@
+import { testingAsserts as ta } from "../deps-test.ts";
+import * as mod from "./axiom.ts";
+import { $ } from "./axiom.ts";
+
+// deno-lint-ignore no-explicit-any
+type Any = any;
+
+// use these for type-testing in IDE
+const $publHost = $.object({
+  host: $.number,
+  mutation_count: $.string.optional(),
+  host_identity: $.string.optional(),
+});
+
+type PublHost = mod.AxiomType<typeof $publHost>;
+const _publHost: PublHost = {
+  host: 0,
+  // bad: "hello"
+};
+
+Deno.test("type-safe data structures at build-time and runtime-testable", async (tc) => {
+  const basicCases: Record<string, [mod.Axiom<Any>, Any[], Any[]]> = {
+    any: [$.any, [1, "", {}, null, undefined], []],
+    array: [$.array(), [[], [1], ["", 1], [{}]], []],
+    arrayTyped: [$.array($.string), [[], [""], ["", "test"]], [
+      [1],
+      ["", 1],
+      undefined,
+    ]],
+    bigint: [$.bigint, [1n, new Object(1n)], [1, "", undefined]],
+    boolean: [$.boolean, [true, false, new Object(true), new Object(false)], [
+      1,
+      0,
+      "",
+      null,
+      undefined,
+      {},
+    ]],
+    custom: [$.custom((value): value is string => typeof value === "string"), [
+      "",
+    ], [1, undefined]],
+    enum: [$.enum("a", "b"), ["a", "b"], ["c", 1, undefined]],
+    function: [$.function, [() => true, function () {}, class Foo {}], [
+      1,
+      {},
+      undefined,
+    ]],
+    instance: [$.instance(RegExp), [/./], ["/./", new Object(), undefined]],
+    intersection: [
+      $.intersection($.object({ foo: $.string }), $.object({ bar: $.number })),
+      [
+        { bar: 1, foo: "" },
+        { bar: 1, baz: true, foo: "" },
+      ],
+      [{ bar: 1 }, { foo: "" }, 1, {}, undefined],
+    ],
+    null: [$.null, [null], [undefined, 1, {}]],
+    number: [$.number, [1, new Object(2)], ["", undefined]],
+    object: [
+      $.object({ bar: $.number, foo: $.string }),
+      [
+        { bar: 1, foo: "" },
+        { bar: 1, baz: true, foo: "" },
+      ],
+      [{ bar: 1 }, { foo: "" }, 1, {}, undefined],
+    ],
+    record: [$.record(), [{}, { foo: "" }, { bar: true, foo: 1 }], [
+      1,
+      null,
+      undefined,
+    ]],
+    recordTyped: [
+      $.record($.string),
+      [{}, { foo: "" }, { bar: "", foo: "" }],
+      [{ foo: 1 }, { bar: 1, foo: "" }, 1, null, undefined],
+    ],
+    string: [$.string, ["", new Object("")], [1, undefined]],
+    symbol: [$.symbol, [Symbol()], [1, {}, null, undefined]],
+    tuple: [$.tuple($.string, $.number), [["", 1]], [
+      ["", 1, true],
+      [],
+      [1, ""],
+      [""],
+      [undefined, 1],
+    ]],
+    undefined: [$.undefined, [undefined], [null, 1, {}]],
+    union: [$.union($.string, $.number), ["", 1], [true, {}, null, undefined]],
+    unknown: [$.unknown, [1, "", {}, null, undefined], []],
+  };
+
+  const preparedSteps: {
+    title: string;
+    test: (t: Deno.TestContext) => void | Promise<void>;
+  }[] = [];
+  Object.entries(basicCases).forEach(([title, [schema, pass, fail]]) => {
+    preparedSteps.push({
+      title,
+      test: () => {
+        pass.forEach((value) => ta.assert(schema.test(value)));
+        pass.forEach((value) => ta.assertEquals(schema.parse(value), value));
+        fail.forEach((value) => ta.assert(!schema.test(value)));
+        fail.forEach((value) => {
+          ta.assertThrows(
+            () => schema.parse(value),
+            Error,
+            "Unexpected type at $",
+          );
+        });
+      },
+    });
+  });
+
+  const partialCases: [string, mod.AxiomObject<Any>][] = [
+    ["object", $.object({ a: $.string }, $.string)],
+    ["record", $.record($.string)],
+  ];
+
+  partialCases.forEach(([name, $object]) => {
+    preparedSteps.push({
+      title: `${name} partial`,
+      test: () => {
+        ta.assert($object.test({ a: "", b: "" }));
+        ta.assert(!$object.test({ a: undefined }));
+        ta.assert(!$object.test({ a: "", b: undefined }));
+        ta.assert(!$object.test({ a: undefined, b: undefined }));
+
+        const $partialObject = $object.partial();
+        ta.assert($partialObject.test({ a: "", b: "" }));
+        ta.assert($partialObject.test({ a: undefined }));
+        ta.assert($partialObject.test({ a: "", b: undefined }));
+        ta.assert($partialObject.test({ a: undefined, b: undefined }));
+        ta.assert($partialObject.test({ a: undefined, b: undefined, c: "" }));
+      },
+    });
+  });
+
+  for await (const step of preparedSteps) {
+    await tc.step(step.title, step.test);
+  }
+
+  await tc.step("optional", () => {
+    ta.assert(!$.string.test(undefined));
+    ta.assert($.string.optional().test(undefined));
+    ta.assertEquals($.string.optional(), $.string.optional());
+  });
+
+  await tc.step("object index", () => {
+    const $object = $.object({ foo: $.string }, $.number);
+    ta.assert($object.test({ foo: "" }));
+    ta.assert($object.test({ bar: 1, foo: "" }));
+    ta.assert(!$object.test({ bar: "", foo: "" }));
+  });
+
+  await tc.step("error", () => {
+    const $object = $.object({
+      "foo bar 😄": $.array($.object({ baz: $.string })),
+    });
+    const value = { "foo bar 😄": [{ baz: 1 }] };
+
+    let onInvalidCalled = 0;
+    let onInvalidCalledWith: string | undefined;
+
+    ta.assert(
+      !$object.test(value, {
+        onInvalid: (reason: string) => {
+          onInvalidCalled++;
+          onInvalidCalledWith = reason;
+        },
+      }),
+    );
+    ta.assertEquals(onInvalidCalled, 1);
+    ta.assertEquals(
+      onInvalidCalledWith,
+      'Unexpected type at $["foo bar 😄"][0].baz',
+    );
+    ta.assertThrows(
+      () => $object.parse(value),
+      Error,
+      'Unexpected type at $["foo bar 😄"][0].baz',
+    );
+  });
+});
