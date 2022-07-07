78ffedf6087a935a4b6a753d4515a7b2d216bc5e
Shahid N. Shah <shahid@shah.org>
2022-07-07 11:22:19 -0400

    fix: remove invalid path from failing test

--- lib/fs/fs-tabular_test.ts
+++ lib/fs/fs-tabular_test.ts
@@ -2,22 +2,14 @@ import { path } from "./deps.ts";
 import { testingAsserts as ta } from "./deps-test.ts";
 import * as mod from "./fs-tabular.ts";
 
-const testPathAbs = path.dirname(import.meta.url).substr("file://".length);
+const testPathAbs = path.dirname(path.fromFileUrl(import.meta.url));
 const _testPathRel = path.relative(Deno.cwd(), testPathAbs);
 
 Deno.test("TODO: file system tabular records", async () => {
   for await (
     const dtr of mod.fileSystemTabularRecords([{
       namespace: "source",
-      // deno-fmt-ignore
-      rootAbsPath: `/home/${Deno.env.get("USER")}/workspaces/gl.infra.medigy.com/medigy-digital-properties/gpm.medigy.com/content`,
-      options: {
-        followSymlinks: true,
-      },
-    }, {
-      namespace: "destination",
-      // deno-fmt-ignore
-      rootAbsPath: `/home/${Deno.env.get("USER")}/workspaces/gl.infra.medigy.com/medigy-digital-properties/gpm.medigy.com/public`,
+      rootAbsPath: testPathAbs,
       options: {
         followSymlinks: true,
       },
