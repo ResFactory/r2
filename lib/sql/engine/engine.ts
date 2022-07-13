d7572a38680aa14b54f2fdb7fac70635c7bf7032
Shahid N. Shah <shahid@shah.org>
2022-07-13 17:56:30 -0400

    feat: introduce SqlReflectConn

--- lib/sql/engine/engine.ts
+++ lib/sql/engine/engine.ts
@@ -1,6 +1,9 @@
 import * as SQLa from "../render/mod.ts";
 import * as ex from "../execute/mod.ts";
 
+// deno-lint-ignore no-explicit-any
+type Any = any;
+
 export type SqlEngineIdentity = string;
 
 export interface SqlEngine {
@@ -53,6 +56,29 @@ export interface SqlReadConn<
       ) => SQLa.SqlTextSupplier<Context>;
     },
   ) => Promise<ex.QueryExecutionRecordSupplier<Object, Context> | undefined>;
+}
+
+export interface SqlReflectConn<
+  Engine extends SqlEngine,
+  Instance extends SqlEngineInstance<Engine>,
+  Context extends SQLa.SqlEmitContext,
+> extends SqlEngineConnection<Engine, Instance> {
+  readonly reflectTables: <TableName extends string>(
+    options?: {
+      readonly filter?: { readonly tableName?: (name: string) => boolean };
+      readonly enrich?: (
+        td: SQLa.TableDefinition<TableName, Context>,
+      ) => SQLa.TableDefinition<TableName, Context>;
+    },
+  ) => Generator<SQLa.TableDefinition<TableName, Context>>;
+  readonly reflectViews: <ViewName extends string>(
+    options?: {
+      readonly filter?: { readonly viewName?: (name: string) => boolean };
+      readonly enrich?: (
+        vd: SQLa.ViewDefinition<ViewName, Context>,
+      ) => SQLa.ViewDefinition<ViewName, Context>;
+    },
+  ) => Generator<SQLa.ViewDefinition<ViewName, Context>>;
 }
 
 export interface SqlWriteConn<
