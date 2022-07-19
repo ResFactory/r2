b04bab4e4b8533f6bae9303c54ba6a96605e6010
Shahid N. Shah <shahid@shah.org>
2022-07-19 17:43:48 -0400

    docs: correct typo

--- lib/text/detect-route.ts
+++ lib/text/detect-route.ts
@@ -7,7 +7,7 @@ const posixPathRE =
  * For example, if you have a navigation utility that needs to set the active
  * path you could run this function to get the component parts (such as name,
  * directory, modifiers, etc.) and find the active page. If there are any extra
- * extensions in the file they are returned as"modifiers".
+ * extensions in the file they are returned as "modifiers".
  * @param text the string to detect and see if it's POSIX-style path
  * returns undefined if it doesn't match a path or components.
  */
